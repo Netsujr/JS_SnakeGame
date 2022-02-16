1f9d39fa77128613c5c0ee8ae32024137d0da8fb
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-16 10:43:52 +0900

    setup snake render logic

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ gameBoard = document.querySelector('#game-board');
 setInterval(function() {
   gameBoard.style.backgroundColor = getRandomColor();
 }, 5000);
+
+// __________________________________________________________
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2;
+
+function main(currentTime) {
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  window.requestAnimationFrame(main);
+  lastRenderTime = currentTime;
+  // console.log(currentTime);
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
